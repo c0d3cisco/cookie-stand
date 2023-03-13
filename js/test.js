63dec54fb7cff3ee6a68006d4a77879143818ddf
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-03-13 18:37:10 -0500

    got first print on page

--- js/test.js
+++ js/test.js
@@ -1,18 +1,17 @@
 
-const hourArrayString = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ', '7pm: ', 'Total: ']; //sets the string of the message and the total hours of the day in an array. Can be automated for user to adjust hours, but needed to hard code for now.
+const hourArrayString = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ', '7pm: ']; //sets the string of the message and the total hours of the day in an array. Can be automated for user to adjust hours, but needed to hard code for now.
 
-// let cityMaster = [
-//   //[city, min/cust, max/cust, avg/sales]
-//   ['Seattle', 23, 65, 6.3],
-//   ['Tokyo', 3, 24, 1.2],
-//   ['Dubai', 11, 38, 3.7],
-//   ['Paris', 20, 38, 6.3],
-//   ['Lima', 2, 65, 6.3],
-// ];
+let cityMaster = [
+  //[city, min/cust, max/cust, avg/sales]
+  ['Seattle', 23, 65, 6.3],
+  ['Tokyo', 3, 24, 1.2],
+  ['Dubai', 11, 38, 3.7],
+  ['Paris', 20, 38, 6.3],
+  ['Lima', 2, 65, 6.3],
+];
 
 
 function hourlyListPrintingFunction(cityObject,elementCreate){
-    let cityName = cityObject.cityName;
   let printArray = cityObject.hourlySale;
   let elementID = cityObject.targetElement;
   let hourlyList = document.getElementById(elementID);
@@ -24,7 +23,6 @@ function hourlyListPrintingFunction(cityObject,elementCreate){
 }
 
 let seattle = { //Varies per city: Min-23, Max-65, AvgCookies-6.3, targetElement - 'city1'
-  cityName: 'Seattle',
   minCustomer: 23,
   maxCustomer: 65,
   avgCookieSaleHour: 6.3,
@@ -37,13 +35,13 @@ let seattle = { //Varies per city: Min-23, Max-65, AvgCookies-6.3, targetElement
       console.log(randomHourly);
       this.hourlySale.push(hourArrayString[i]+Math.ceil(randomHourly*this.avgCookieSaleHour)+' cookies'); //recommend to Math.ceil to always round up. Better to have one extra cookie than one too few cookies.
     }
-
   },
   printList:[],
 
 };
 seattle.hourlyFunc();
-hourlyListPrintingFunction(seattle,'li');
+hourlyListPrintingFunction(seattle,'p');
+
 
 
 
